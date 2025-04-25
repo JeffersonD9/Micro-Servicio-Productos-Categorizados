5c044baa2b0032e2468b469b4db8b7c1fbed8d24
Jefferson <jeffersonm0922@gmail.com>
2025-04-24 23:33:50 -0500

    Configure verytoken

--- src/midedlewares/VerifyToken.js
+++ src/midedlewares/VerifyToken.js
@@ -1,33 +1,22 @@
-// middlewares/verifyToken.js
 import jwt from 'jsonwebtoken';
+import { SECRET_TOKEN } from '../config.js'; // Asegúrate de importar la constante
 
 export const verifyToken = (req, res, next) => {
   try {
-    // Obtener el token del encabezado Authorization
     const authHeader = req.headers.authorization;
-    console.log("auth token",authHeader)
-    
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
-      return res.status(401).json({ 
-        message: 'Acceso denegado. Token no proporcionado o formato incorrecto' 
-      });
+      return res.status(401).json({ message: 'Acceso denegado. Token no proporcionado o formato incorrecto' });
     }
-    
-    // Extraer el token (quitar el prefijo "Bearer ")
+
     const token = authHeader.split(' ')[1];
-    console.log(token)
-    // Verificar el token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    
-    // Agregar la información del usuario decodificada a la solicitud
+    const decoded = jwt.verify(token, SECRET_TOKEN); 
+
     req.user = decoded;
-    
     next();
   } catch (error) {
     if (error.name === 'TokenExpiredError') {
       return res.status(401).json({ message: 'El token ha expirado' });
     }
-  
     return res.status(401).json({ message: 'Token inválido' });
   }
 };

69e6bd91e75ef3d0aa7d3e8645d4f64b2262b6a2
SteveDev <97489933+JeffersonD9@users.noreply.github.com>
2025-05-07 15:43:51 -0500

    Update Category.js

--- src/Helpers/Category.js
+++ src/Helpers/Category.js
@@ -7,6 +7,14 @@ class Category extends CRUDModel{
     constructor(){
         super(prisma.category)
     }
+    
+async function UncategorizedProducts(id_category) {
+    const result = await prisma.product.updateMany({
+        where: { id_Category: id_category },
+        data: { id_Category: 0 }
+    });
+    return result;
+}
 }
 
 export const CategoryService = new Category();
